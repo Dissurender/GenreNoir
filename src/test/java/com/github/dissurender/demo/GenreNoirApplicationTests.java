17e71cf6a67a9996b28efb83ad63be7448ac0e56
diss <65290988+Dissurender@users.noreply.github.com>
2023-10-03 10:40:58 -0500

    initializr files added

---
+++ src/test/java/com/github/dissurender/demo/GenreNoirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dissurender.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenreNoirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
